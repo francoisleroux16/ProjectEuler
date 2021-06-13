c1f848d27bc98a309476cc92e9fbc927792f0039
fleroux@mit.edu <fleroux@mit.edu>
2021-06-13 20:37:24 +0200

    Local Clone

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import numpy as np
 def question_1():
   ans = 24
   return ans
+
+def question_2():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    ans = 15*np.sin(15)
+    return ans
